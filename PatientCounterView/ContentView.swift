40d830a4dee32e8d6ab4700cd6725cafb533e4a3
jonathan strid <pjstrid@outlook.com>
2026-04-17 13:05:06 +0100

    Initial Commit

---
+++ PatientCounterView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PatientCounterView
+//
+//  Created by Jonathan Strid on 2026-04-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PatientCounterView/PatientCounterViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatientCounterViewApp.swift
+//  PatientCounterView
+//
+//  Created by Jonathan Strid on 2026-04-17.
+//
+
+import SwiftUI
+
+@main
+struct PatientCounterViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
